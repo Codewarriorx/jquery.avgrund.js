0f8a7ea80df047185fe4d7de3d8cff231f26563d
Michael Lauridsen <michael@saunders.rit.edu>
2013-04-02 14:03:27 -0400

    Added public destroy function
    This function can be called anytime to deactivate the plugin. Also added
    structure to add other functions in the future.

--- js/jquery.avgrund.js
+++ js/jquery.avgrund.js
@@ -6,7 +6,7 @@
  */
 
 (function($) {
-	$.fn.avgrund = function(options) {
+	$.fn.avgrund = function(method) {
 		var defaults = {
 			width: 380, // max = 640
 			height: 280, // max = 350
@@ -24,117 +24,118 @@
 			onUnload: function() {},
 			template: '<p>This is test popin content!</p>'
 		};
-		var options = $.extend(defaults, options);
 
-		return this.each(function() {
-			var self = $(this),
-				body = $('body'),
-				maxWidth = options.width > 640 ? 640 : options.width,
-				maxHeight = options.height > 350 ? 350 : options.height,
-				template = typeof options.template === 'function' ? options.template(self) : options.template;
+		var methods = {
+			init: function(options){
+				var options = $.extend(defaults, options);
 
-			body.addClass('avgrund-ready');				
-			
-			if (options.onBlurContainer !== '') {
-				$(options.onBlurContainer).addClass('avgrund-blur');
-			}
-			
-			// close popup by clicking Esc button
-			function onDocumentKeyup(e) {
-				if (options.closeByEscape) {
-					if (e.keyCode === 27) {
-						deactivate();
+				return this.each(function() {
+					var self = $(this),
+						body = $('body'),
+						maxWidth = options.width > 640 ? 640 : options.width,
+						maxHeight = options.height > 350 ? 350 : options.height,
+						template = typeof options.template == 'function' ? options.template(self) : options.template;
+
+					body.addClass('avgrund-ready');
+					body.append('<div class="avgrund-overlay ' + options.overlayClass + '"></div>');				
+					body.append('<div class="avgrund-popin ' + options.holderClass + '">' + template + '</div>');
+
+					$('.avgrund-popin').css({
+						'width': maxWidth + 'px',
+						'height': maxHeight + 'px',
+						'margin-left': '-' + (maxWidth / 2 + 10) + 'px',
+						'margin-top': '-' + (maxHeight / 2 + 10) + 'px'
+					});
+
+					if (options.showClose) {
+						$('.avgrund-popin').append('<a href="#" class="avgrund-close">' + options.showCloseText + '</a>');
 					}
-				}
-			}
-			
-			// close popup by clicking outside it
-			function onDocumentClick(e) {
-				if (options.closeByDocument) {
-					if ($(e.target).is('.avgrund-overlay, .avgrund-close')) {
-						deactivate();
-					}
-				} else {
-					if ($(e.target).is('.avgrund-close')) {
-						deactivate();
-					}
-				}
-			}
 
-			// show popup
-			function activate() {
-				// check if onLoad is a function and call it before popin is active
-				if (typeof options.onLoad === 'function') {
-					options.onLoad.call(self);
-				}
+					if (options.enableStackAnimation) {
+						$('.avgrund-popin').addClass('stack');
+					}
 
-				setTimeout(function() {
-					body.addClass('avgrund-active');
-				}, 100);
-				
-				body.append('<div class="avgrund-overlay ' + options.overlayClass + '"></div>');
-				body.append('<div class="avgrund-popin ' + options.holderClass + '">' + template + '</div>');				
-				
-				$('.avgrund-popin').css({
-					'width': maxWidth + 'px',
-					'height': maxHeight + 'px',
-					'margin-left': '-' + (maxWidth / 2 + 10) + 'px',
-					'margin-top': '-' + (maxHeight / 2 + 10) + 'px'
+					if (options.onBlurContainer != '') {
+						$(options.onBlurContainer).addClass('avgrund-blur');
+					}
+					
+					// close popup by clicking Esc button
+					function onDocumentKeyup(e) {
+						if (options.closeByEscape) {
+							if (e.keyCode === 27) {
+								deactivate();
+							}
+						}
+					}
+					
+					// close popup by clicking outside it
+					function onDocumentClick(e) {
+						if (options.closeByDocument) {
+							if ($(e.target).is('.avgrund-overlay, .avgrund-close')) {
+								deactivate();
+							}
+						} else {
+							if ($(e.target).is('.avgrund-close')) {
+								deactivate();
+							}	
+						}
+					}
+
+					// show popup
+					function activate() {
+						// check if onLoad is a function and call it before popin is active
+						if (typeof options.onLoad == 'function') {
+							options.onLoad.call(self);
+						}
+
+						body.bind('keyup', onDocumentKeyup);
+						body.bind('click', onDocumentClick);
+
+						body.addClass('avgrund-active');
+					}
+
+					// hide popup
+					function deactivate() {
+						body.unbind('keyup', onDocumentKeyup);
+						body.unbind('click', onDocumentClick);
+
+						body.removeClass('avgrund-active');
+
+						// check if onUnload is a function and call it after popin is closed
+						if (typeof options.onUnload == 'function') {
+							options.onUnload.call(self);
+						}
+					}
+
+					// init on click or custom event
+					if (options.openOnEvent) {
+						self.bind(options.setEvent, function(e) {
+							e.stopPropagation();
+							activate();
+						});
+					} else {
+						activate();
+					}
 				});
-
-				if (options.showClose) {
-					$('.avgrund-popin').append('<a href="#" class="avgrund-close">' + options.showCloseText + '</a>');
-				}
-
-				if (options.enableStackAnimation) {
-					$('.avgrund-popin').addClass('stack');
-				}
-
-				// fixing -webkit overlay 'transform/position:fixed/overflow' issue 
-				body.wrapInner('<div class="avgrund-wrap-inner" />');
-
-				body.bind('keyup', onDocumentKeyup);
-				body.bind('click', onDocumentClick);
+			},
+			destroy: function(){ // public function to deactivate modal
+				$('body').unbind();
+				$('body').removeClass('avgrund-active');
 			}
-
-			// hide popup
-			function deactivate() {
-				body.unbind('keyup', onDocumentKeyup);
-				body.unbind('click', onDocumentClick);
-
-				body.removeClass('avgrund-active');
-
-				// prevent multiple overlays
-				$('.avgrund-overlay').remove();
-				
-				// remove after small pause to apply special avgrund effect
-				setTimeout(function() {
-					$('.avgrund-popin').remove();
-					$('.avgrund-wrap-inner > *').unwrap();
-				}, 500);
-
-				// check if onUnload is a function and call it after popin is closed
-				if (typeof options.onUnload === 'function') {
-					options.onUnload.call(self);
-				}
-			}
-
-			// init on click or custom event
-			if (options.openOnEvent) {
-				self.bind(options.setEvent, function(e) {
-					e.stopPropagation();
-
-					// prevent redirect for href url
-					if ($(e.target).is('a')) {
-						e.preventDefault();
-					}
-
-					activate();
-				});
-			} else {
-				activate();
-			}
-		});
+		}
+		// if a method as the given argument exists
+		if (methods[method]) {
+		    // call the respective method
+		    return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		// if an object is given as method OR nothing is given as argument
+		} else if (typeof method === 'object' || !method) {
+		    // call the initialization method
+		    return methods.init.apply(this, arguments);
+		// otherwise
+		} else {
+		    // trigger an error
+		    $.error( 'Method "' +  method + '" does not exist in pluginName plugin!');
+		}
 
 	}
 })(jQuery)
